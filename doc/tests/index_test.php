2ad5952e6335df66d07cfa8dfff952b3dd348bc0
Romain <romain@MacBook-Pro-de-Romain.local>
2016-11-20 16:45:53 +0100

    debut modification selenium2

--- doc/tests/index_test.php
+++ doc/tests/index_test.php
@@ -1,22 +1,23 @@
 <?php
-class Example extends PHPUnit_Extensions_SeleniumTestCase
+include_once('conf.php');
+class Example extends PHPUnit_Extensions_Selenium2TestCase
 {
   protected function setUp()
   {
-    $this->setBrowser("*chrome");
-    $this->setBrowserUrl("http://localhost/");
+    $this->setBrowser("chrome");
+    $this->setBrowserUrl($GLOBALS['serverPath']);
   }
 
-  public function testMyTestCase()
+  public function testElements()
   {
-    $this->open("http://localhost/ScrumDev/src/web/signin.php");
-    $this->assertEquals("Scrumify", $this->getTitle());
-    $this->assertEquals("Connexion", $this->getText("css=h2.text-center"));
-    $this->assertTrue($this->isElementPresent("link=Connexion"));
-    $this->assertTrue($this->isElementPresent("link=Inscription"));
-    $this->verifyText("//label[text()='Email']", "Email");
-    $this->verifyText("//label[text()='Password']", "Password");
-    $this->assertTrue($this->isElementPresent("name=submit"));
+      $this->url("signin.php");
+      $this->assertEquals("Scrumify", $this->title());
+      $this->assertEquals("Connexion", $this->byCssSelector("h2.text-center")->text());
+      $this->assertTrue($this->byLinkText("Connexion")->size()!=0);
+      $this->assertTrue($this->byLinkText("Inscription")->size()!=0);
+      $this->assertTrue($this->byName('email')->size()!=0);
+      $this->assertTrue($this->byName('password')->size()!=0);
+      $this->assertTrue($this->byName('submit')->size()!=0);
   }
 }
 ?>
